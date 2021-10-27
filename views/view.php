18c76d5478ecb55fd2c172d4a9d1b34a6f6e554a
Shafik AlNahhas <mhd.shafi2.n@gmail.com>
2021-10-27 15:44:17 +0300

    finishing creating the view meeting

--- views/view.php
+++ views/view.php
@@ -5,6 +5,7 @@
  */
 init_head();
 ?>
+
 <div id="wrapper">
     <div class="content">
         <div class="row">
@@ -23,35 +24,49 @@ init_head();
                                 <hr>
                             </div>
                             <div class="form-group">
-                                <h4><strong>Description:</strong> <?= $meeting["bodyPreview"] ?></h4>
+                                <h4><strong>Description:</strong>
+                                    <?php
+                                    $position = strpos($meeting["bodyPreview"], "\r\n");
+                                    $result = substr($meeting["bodyPreview"], 0, $position);
+                                    echo $result;
+                                    ?>
+                                </h4>
                                 <hr>
                             </div>
                             <div class="form-group">
-                                <h4 class="<?= (ucfirst($meeting->status) === 'Started') ? 'text-success' : 'text-info' ?>"
-                                    data-toggle="tooltip"
-                                    title="After clicking on the Start URL your browser will open new tab, after the tab is fully loaded you can close the tab. Then you can join the meeting by click in Join URL(Web)">
-                                    <strong>Status:</strong>
-                                    <?= ucfirst($meeting->status) ?>
-                                    <?php if ($meeting->status === 'waiting') : ?>
-                                    <a class="pull-right" href="<?= $meeting->start_url; ?>"
-                                        target="_blank"><strong>Start URL</strong></a>
-                                    <?php endif; ?>
+                                <h4><strong>Meeting Date:</strong>
+                                    <?php
+                                    $position = strpos($meeting["start"]["dateTime"], 'T');
+                                    $result = substr($meeting["start"]["dateTime"], 0, $position);
+                                    echo $result;
+                                    ?>
                                 </h4>
                                 <hr>
                             </div>
                             <div class="form-group">
                                 <h4><strong>Start Time:</strong>
-                                    <?= _dt($meeting["start"]["dateTime"]) ?></h4>
+                                    <?php
+                                    $position1 = strpos($meeting["start"]["dateTime"], 'T');
+                                    $result = substr($meeting["start"]["dateTime"], $position1 + 1, 5);
+                                    echo $result;
+                                    ?>
+                                </h4>
                                 <hr>
                             </div>
                             <div class="form-group">
-                                <h4><strong>Duration:</strong>
+                                <h4><strong>End Time:</strong>
                                     <?php
-                                    $startDate = new DateTime($meeting["start"]["dateTime"]);
-                                    $endDate = new DateTime($meeting["end"]["dateTime"]);
-                                    $duration = $startDate->diff($endDate);
+                                    $position1 = strpos($meeting["end"]["dateTime"], 'T');
+                                    $result = substr($meeting["end"]["dateTime"], $position1 + 1, 5);
+                                    echo $result;
                                     ?>
-                                    <?= $duration->h . " hours " . $duration->m . " minutes "; ?></h4>
+                                    <!-- <?php
+                                            $startDate = new DateTime($meeting["start"]["dateTime"]);
+                                            $endDate = new DateTime($meeting["end"]["dateTime"]);
+                                            $duration = $startDate->diff($endDate);
+                                            ?>
+                                <?= $duration->h . " hours " . $duration->m . " minutes "; ?> -->
+                                </h4>
                                 <hr>
                             </div>
                             <div class="form-group">
@@ -61,29 +76,43 @@ init_head();
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
-                                <h4><strong>Meeting Type:</strong>
-                                    <?= $meeting["type"] ?></h4>
-                                <hr>
-                            </div>
-                            <div class="form-group">
-                                <h4><strong>Allow participants to join the meeting before the host starts the
-                                        meeting.:</strong>
-                                    Yes</h4>
-                                <hr>
-                            </div>
-                            <div class="form-group">
                                 <h4>
-                                    <a href="<?= str_replace('j/', 'wc/join/', $meeting["onlineMeeting"]["joinUrl"]); ?>"
-                                        target="_blank">
+                                    <a href="<?= str_replace('j/', 'wc/join/', $meeting["onlineMeeting"]["joinUrl"]); ?>" target="_blank">
                                         <strong>Join URL</strong>
                                     </a>
                                 </h4>
                                 <hr>
                             </div>
+                            <div class="form-group">
+                                <h4><strong>Meeting Type:</strong>
+                                    <?= $meeting["type"] ?></h4>
+                                <hr>
+                            </div>
+                            <div class="form-group">
+                                <h4><strong>Organizer:</strong>
+                                    <?= $meeting["organizer"]["emailAddress"]["name"] ?></h4>
+                                <hr>
+                            </div>
+                            <div class="form-group">
+                                <h4><strong>Allow participants to join the meeting before the host starts the
+                                        meeting:</strong>
+                                    Yes</h4>
+                                <hr>
+                            </div>
+
+                            <div class="form-group">
+                                <h4><strong>Attendees:</strong>
+                                    <?php
+                                    foreach ($meeting["attendees"] as $attend) {
+                                        echo '<br>' . $attend["emailAddress"]["name"];
+                                    }
+
+                                    ?>
+                                    <hr>
+                            </div>
                         </div>
                         <div class="clearfix"></div>
-                        <a href="<?= admin_url('teams_meeting_manager/meetings/index'); ?>"
-                            class="btn btn-default btn-xs">Back To Meetings</a>
+                        <a href="<?= admin_url('teams_meeting_manager/meetings/index'); ?>" class="btn btn-default btn-xs">Back To Meetings</a>
                     </div>
                 </div>
             </div>
@@ -93,10 +122,10 @@ init_head();
 </div>
 <?php init_tail(); ?>
 <script>
-/**
- * Just toggles the menu to be active
- */
-$('.menu-item-zoom_meeting_manager').addClass('active');
+    /**
+     * Just toggles the menu to be active
+     */
+    $('.menu-item-teams_meeting_manager').toggleClass('active');
 </script>
 </body>
 
